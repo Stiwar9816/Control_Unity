cdbf6a92b86b3d5f483350d8dcbc6c90126945da
Stiwar Asprilla <Stiwar Asprilla@DESKTOP-S7URQ0I>
2018-12-07 17:22:47 -0500

    Creacion de la capa de negocio

---
+++ Contro_unity/CapaDatos/CDconexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    class CDconexion
+    {
+        private SqlConnection conexion = new SqlConnection("Server= DESKTOP-S7URQ0I;DataBase=control_unity;Integrated Security=true");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+    }
+}
---
+++ Contro_unity/CapaDatos/CDlogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    
+    public class CDlogin
+    {
+        private CDconexion conexion = new CDconexion();
+        private SqlDataAdapter leer;
+
+        public SqlDataAdapter IniciarSesion(string user, string pass)
+        {
+            string sql = "SELECT * FROM users WHERE cc_user=" + user + "AND password_user=" + pass;
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = sql;
+            leer = comando.ExecuteReader();
+            return leer;
+        }
+    }
+}
